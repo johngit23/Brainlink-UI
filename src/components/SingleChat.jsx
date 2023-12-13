b871fd9709fe788a123ee32d02b14461d1bd1a1b
johngit23 <126653377+johngit23@users.noreply.github.com>
2023-12-13 12:57:17 +0300

    Update SingleChat.jsx

--- src/components/SingleChat.jsx
+++ src/components/SingleChat.jsx
@@ -15,7 +15,7 @@ import animationData from "../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "http://localhost:3000"; // "https://brain-link.herokuapp.com"; -> After deployment
+const ENDPOINT = "https://brainlink-api-8p5w.onrender.com"; // "https://brain-link.herokuapp.com"; -> After deployment
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
