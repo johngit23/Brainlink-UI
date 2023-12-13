2b1a145b11c54e6bd9faf69ac329136fb8939ebf
johngit23 <126653377+johngit23@users.noreply.github.com>
2023-12-13 12:55:20 +0300

    Update UpdateGroupChatModal.jsx

--- src/components/miscellaneous/UpdateGroupChatModal.jsx
+++ src/components/miscellaneous/UpdateGroupChatModal.jsx
@@ -47,7 +47,7 @@ const UpdateGroupChatModal = ({ fetchMessages, fetchAgain, setFetchAgain }) => {
         },
       };
       const { data } = await axios.get(
-        `http://localhost:3000/api/user?search=${search}`,
+        `https://brainlink-api-8p5w.onrender.com/api/user?search=${search}`,
         config
       );
       console.log(data);
@@ -77,7 +77,7 @@ const UpdateGroupChatModal = ({ fetchMessages, fetchAgain, setFetchAgain }) => {
         },
       };
       const { data } = await axios.put(
-        `http://localhost:3000/api/chat/rename`,
+        `https://brainlink-api-8p5w.onrender.com/api/chat/rename`,
         {
           chatId: selectedChat._id,
           chatName: groupChatName,
@@ -135,7 +135,7 @@ const UpdateGroupChatModal = ({ fetchMessages, fetchAgain, setFetchAgain }) => {
         },
       };
       const { data } = await axios.put(
-        `http://localhost:3000/api/chat/groupadd`,
+        `https://brainlink-api-8p5w.onrender.com/api/chat/groupadd`,
         {
           chatId: selectedChat._id,
           userId: user1._id,
@@ -180,7 +180,7 @@ const UpdateGroupChatModal = ({ fetchMessages, fetchAgain, setFetchAgain }) => {
         },
       };
       const { data } = await axios.put(
-        `http://localhost:3000/api/chat/groupremove`,
+        `https://brainlink-api-8p5w.onrender.com/api/chat/groupremove`,
         {
           chatId: selectedChat._id,
           userId: user1._id,
