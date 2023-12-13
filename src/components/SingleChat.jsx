141b92982c4db215320795014677f5e4883e6e56
johngit23 <126653377+johngit23@users.noreply.github.com>
2023-12-13 12:56:40 +0300

    Update SingleChat.jsx

--- src/components/SingleChat.jsx
+++ src/components/SingleChat.jsx
@@ -62,7 +62,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
       setLoading(true);
 
       const { data } = await axios.get(
-        `http://localhost:3000/api/message/${selectedChat._id}`,
+        `https://brainlink-api-8p5w.onrender.com/api/message/${selectedChat._id}`,
         config
       );
       setMessages(data);
@@ -93,7 +93,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
         };
         setNewMessage("");
         const { data } = await axios.post(
-          "http://localhost:3000/api/message",
+          "https://brainlink-api-8p5w.onrender.com/api/message",
           {
             content: newMessage,
             chatId: selectedChat._id,
